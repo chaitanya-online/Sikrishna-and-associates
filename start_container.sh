6b90860d126e99bd86e1cd759eaa7d0d2775bb1c
Chaitanya Nampalli <chaitanyamurali.nampalli@serole.com>
2024-04-26 15:07:41 +0530

    changes

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-# git pull
-git pull
+
 
 # Define the container name
 CONTAINER_NAME="cabalu-website-container"
@@ -10,24 +9,32 @@ CONTAINER_NAME="cabalu-website-container"
 # Define image name
 IMAGE_NAME="richeb/cabalu-website:v1"
 
-# docker build
+
+# 1 git pull
+echo " Started pulling from SCM "
+
+git pull
+
+echo "Pulled Sucessfully"
+
+# 2 docker build
 
 docker build --no-cache -t "$IMAGE_NAME" .
 
 
-# Pushing image to docker hub
+# 3 Pushing image to docker hub
 docker push  "$IMAGE_NAME"
 
-# Check if the container already exists
+# 4 Check if the container already exists
 if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
     # If the container exists, stop and remove it
     docker stop "$CONTAINER_NAME"
     docker rm "$CONTAINER_NAME"
 fi
 
-# Pull the Docker image from Docker Hub 
+#5  Pull the Docker image from Docker Hub 
 docker pull "$IMAGE_NAME"
 
-# Run the Docker image as a container
+#6 Run the Docker image as a container
 
 docker run -p 80:80 --name "$CONTAINER_NAME" -d "$IMAGE_NAME"
