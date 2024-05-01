2a7e47efe2082a4435084087b72ef8d6290748f0
Chaitanya Nampalli <chaitanyamurali.nampalli@serole.com>
2024-05-01 12:15:19 +0530

    changes

--- start_container.sh
+++ start_container.sh
@@ -25,6 +25,9 @@ docker build --no-cache -t "$IMAGE_NAME" .
 # 3 Pushing image to docker hub
 docker push  "$IMAGE_NAME"
 
+echo "image pushed to docker hub"
+
+
 # 4 Check if the container already exists
 if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
     # If the container exists, stop and remove it
@@ -38,3 +41,14 @@ docker pull "$IMAGE_NAME"
 #6 Run the Docker image as a container
 
 docker run -p 80:80 --name "$CONTAINER_NAME" -d "$IMAGE_NAME"
+
+echo "container is up and running"
+
+
+
+#7 deleting unused docker images
+
+docker system prune
+
+echo "Deleted old data"
+
